a79b3086b494ca553465a04c3bf52114f2f66f12
tanya <tanya>
2021-09-22 17:56:28 +0300

    added init DB

---
+++ app/models/index.js
@@ -0,0 +1,29 @@
+const Datastore = require("nedb");
+const Permissions = require("./../../common/models/permissions").permissionsData;
+
+let Meals = new Datastore();
+let Menus = new Datastore();
+let Users = new Datastore();
+
+
+usersData = [
+  { Username: "michael", Password: "123", Permission: Permissions.Chef},
+  { Username: "ahmed", Password: "1as", Permission: Permissions.KitchenWorker},
+  { Username: "refael", Password: "asd", Permission: Permissions.Soldier},
+];
+
+
+let initDBData = (data, db) => {
+  for (const datum of data) {
+    db.insert(datum);
+  }
+}
+
+initDBData(usersData, Users);
+
+module.exports = {
+  Meals,
+  Menus,
+  Users,
+  Permissions
+};
---
+++ common/models/permissions.js
@@ -0,0 +1,7 @@
+const permissionsData = {
+  Soldier: 222,
+  KitchenWorker: 444,
+  Chef: 777
+}
+
+module.exports = {permissionsData};
