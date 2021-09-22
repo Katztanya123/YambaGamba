f60123f36ef4e4a3f06e8623b3de46d47213d033
tanya <tanya>
2021-09-22 19:34:44 +0300

    created db files

--- common/models/db.js
+++ common/models/db.js
@@ -1,9 +1,10 @@
 const Datastore = require("nedb");
 const Permissions = require("./permissions").permissionsData;
+const path = require('path');
 
-let Meals = new Datastore();
-let Menus = new Datastore();
-let Users = new Datastore();
+let Meals = new Datastore({filename: path.join(__dirname, './dbs/Meals.db'), autoload: true});
+let Menus = new Datastore({filename: path.join(__dirname, './dbs/Menus.db'), autoload: true});
+let Users = new Datastore({filename: path.join(__dirname, './dbs/Users.db'), autoload: true});
 
 
 usersData = [
@@ -19,7 +20,7 @@ let initDBData = (data, db) => {
   }
 }
 
-initDBData(usersData, Users);
+//initDBData(usersData, Users);
 
 module.exports = {
   Meals,
